bfde7aa060b63376ab55889fe14ae002e3076367
LeandroReichardt <163359722+LeandroReichardt@users.noreply.github.com>
2024-12-11 00:13:31 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import Calculadora from './components/Calculadora';
+
+function App(params) {
+
+
+    return (
+
+            
+        
+            <Calculadora />
+
+            
+
+    )
+    
+}
+
+
+export default App
+
